54dd5135a64b14b98ea79c58ceee611e2f58fff5
Marc C <m.ciborowski@hotmail.com>
2020-05-29 00:36:34 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -42,6 +42,7 @@ def turn(board)
     index = input_to_index(input)
   if valid_move?(board, index) == true
     move(board, index, player = "X")
+    display_board(board)
   else
     turn(board)
   end
