3e2203b2e1a7f6cd0557e12bf4c2858acabb7b7a
Marc C <m.ciborowski@hotmail.com>
2020-05-29 00:28:25 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -40,7 +40,6 @@ def turn(board)
     puts "Please enter 1-9:"
     input = gets.strip
     index = input_to_index(input)
-    valid_move?(board, index)
   if valid_move?(board, index) == true
     move(board, index, player = "X")
   else
